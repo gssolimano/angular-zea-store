9000e127e5ccd2f4535c0d79a34b3cdb542fdc34
gssolimano <gssolimano@gmail.com>
2019-12-07 00:54:38 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-zea-store
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-zea-store)
---
+++ src/app/products.ts
@@ -0,0 +1,26 @@
+export const products = [
+  {
+    name: "Body nena",
+    price: 210.5,
+    description: "Body nena talles 0 al 4",
+    talle: [0, 1, 2, 3, 4]
+  },
+  {
+    name: "Pantalon nena",
+    price: 190,
+    description: "Pantalon nena talles 0 al 4",
+    talle: [1, 2, 4]
+  },
+  {
+    name: "Campera reversible",
+    price: 380.95,
+    description: "Campera reversible talles 0 al 6",
+    talle: [0, 1, 4]
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
